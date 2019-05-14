92251786d1d589e41e6d70a16da2d700821df894
Keith Wolf <wolftrain@hotmail.com>
2019-05-13 21:20:08 -0700

    forgot to save

--- src/main/java/com/wolftrain/app/TacoController.java
+++ src/main/java/com/wolftrain/app/TacoController.java
@@ -1,5 +1,11 @@
 package main.java.com.wolftrain.app;
 
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TacoController {
 
 }
